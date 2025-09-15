248b4ace057c2d8171e493143da2913d1942ee2f
nikolamaksic <nikola7amg@gmail.com>
2025-09-15 02:31:58 +0200

    feat: update file version view set

--- src/propylon_document_manager/file_versions/api/views.py
+++ src/propylon_document_manager/file_versions/api/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..models import FileVersion
 from .serializers import FileVersionSerializer
 
 class FileVersionViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
-    authentication_classes = []
-    permission_classes = []
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = FileVersionSerializer
     queryset = FileVersion.objects.all()
     lookup_field = "id"
+
+    def get_queryset(self):
+        print(self.request.user)
+        print("USAAAAAAAAAOOOOOOOOO")
+        """Limit to files owned by the authenticated user."""
+        return self.queryset.filter(base_file__owner_id=self.request.user.id).order_by("-id")
